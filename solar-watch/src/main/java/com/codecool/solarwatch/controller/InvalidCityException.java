8eebce68956d30e0955cde01ab40524b37f757cf
Péter Gubicza <132732117+petergubicza@users.noreply.github.com>
2024-02-07 17:02:35 +0100

    Delete solar-watch directory

--- solar-watch/src/main/java/com/codecool/solarwatch/controller/InvalidCityException.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.solarwatch.controller;
-
-public class InvalidCityException extends RuntimeException {
-    public InvalidCityException() {
-        super("City does NOT exist");
-    }
-}
-
